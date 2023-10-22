4de697fefdd6a8278c61d4e45d0c24434707e337
Hassaan Muhammad <hassaans299@gmail.com>
2023-10-22 00:50:34 -0700

    initial

---
+++ dist/index.js
@@ -0,0 +1,3 @@
+"use strict";
+console.log("Typescript is such a bitch");
+console.log("Typescript is not such a bitch");
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+console.log("Typescript is such a bitch")
+console.log("Typescript is not such a bitch")
